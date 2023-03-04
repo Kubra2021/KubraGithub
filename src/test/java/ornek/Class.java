379b66bba381142302002c42c39b74c0aded2dbc
Kubra <you@example.com>
2023-03-04 21:14:18 +0100

    first commit

---
+++ src/test/java/ornek/Class.java
@@ -0,0 +1,16 @@
+package ornek;
+
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("Github calismasi yapildi");
+    }
+}
+
+        /*
+        README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/Kubra2021/KubraGithub.git
+        git push -u origin main
+
+     */
